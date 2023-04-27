151e0ad528675fd593bf2783c0caccbc18e9fb9a
BoRa <bora.simsekyilmaz@gmail.com>
2023-04-27 06:57:52 +0300

    .
    .

--- Server/Server/GameHandler.cs
+++ Server/Server/GameHandler.cs
@@ -4,7 +4,6 @@ using SimpleTCP;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
